a4a72e7ffe63370a5b4046e62a1bafa2e5e37c6e
Michal Czyzewski <czy.mich@gmail.com>
2019-10-18 15:16:22 +0200

    add files variables in Recipe class

--- src/main/java/pl/mczyzewski/recipeapp/domain/Recipe.java
+++ src/main/java/pl/mczyzewski/recipeapp/domain/Recipe.java
@@ -1,4 +1,15 @@
 package pl.mczyzewski.recipeapp.domain;
 
 public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty;
+    private Byte[] image;
 }
