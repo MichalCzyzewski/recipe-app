d03a8ac753371dfffef4cc95b9e941e414193bd3
Michal Czyzewski <czy.mich@gmail.com>
2019-10-19 00:37:36 +0200

    add class Ingredient

---
+++ src/main/java/pl/mczyzewski/recipeapp/domain/Ingredient.java
@@ -0,0 +1,11 @@
+package pl.mczyzewski.recipeapp.domain;
+
+import java.math.BigDecimal;
+
+public class Ingredient {
+
+    private String description;
+    private BigDecimal amount;
+
+    //privet unitOfMeasure uom;
+}
