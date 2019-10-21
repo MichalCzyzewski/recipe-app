c1be805feadbd9e887c6ebf99127d8ad266b96e2
Michal Czyzewski <czy.mich@gmail.com>
2019-10-21 22:20:40 +0200

    add data.sql source file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-IDENTITY INTO category (description) VALUE ('America');
-IDENTITY INTO category (description) VALUE ('Italian');
-IDENTITY INTO category (description) VALUE ('Mexican');
-IDENTITY INTO category (description) VALUE ('Fast Food');
-IDENTITY INTO unit_of_measure (description) VALUE ('Teaspoon');
-IDENTITY INTO unit_of_measure (description) VALUE ('Tablespoon');
-IDENTITY INTO unit_of_measure (description) VALUE ('Cup');
-IDENTITY INTO unit_of_measure (description) VALUE ('Pinch');
-IDENTITY INTO unit_of_measure (description) VALUE ('Ounce');
+INSERT INTO CATEGORY (DESCRIPTION) VALUES ('American');
+INSERT INTO CATEGORY (DESCRIPTION) VALUES ('Italian');
+INSERT INTO CATEGORY (DESCRIPTION) VALUES ('Mexican');
+INSERT INTO CATEGORY (DESCRIPTION) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
