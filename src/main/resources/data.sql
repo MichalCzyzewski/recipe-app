46023d4b10d07132ac114f390eeac3b1cd27c26c
Michal Czyzewski <czy.mich@gmail.com>
2019-10-21 21:41:50 +0200

    add data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+IDENTITY INTO category (description) VALUE ('America');
+IDENTITY INTO category (description) VALUE ('Italian');
+IDENTITY INTO category (description) VALUE ('Mexican');
+IDENTITY INTO category (description) VALUE ('Fast Food');
+IDENTITY INTO unit_of_measure (description) VALUE ('Teaspoon');
+IDENTITY INTO unit_of_measure (description) VALUE ('Tablespoon');
+IDENTITY INTO unit_of_measure (description) VALUE ('Cup');
+IDENTITY INTO unit_of_measure (description) VALUE ('Pinch');
+IDENTITY INTO unit_of_measure (description) VALUE ('Ounce');
