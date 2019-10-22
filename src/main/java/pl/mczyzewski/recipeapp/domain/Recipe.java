e65a41681c5a44d4a21e4c3ac351b73b3dbe4d23
Michal Czyzewski <czy.mich@gmail.com>
2019-10-22 13:54:49 +0200

    add @Lob to directions in Recipe class

--- src/main/java/pl/mczyzewski/recipeapp/domain/Recipe.java
+++ src/main/java/pl/mczyzewski/recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package pl.mczyzewski.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -15,12 +16,13 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
+
     @Lob
     private Byte[] image;
 
