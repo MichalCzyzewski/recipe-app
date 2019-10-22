ea39dba1523f7dc0365817e752ac5940cf23f431
Michal Czyzewski <czy.mich@gmail.com>
2019-10-22 11:37:09 +0200

    add findByDescription in CategoryRepository

--- src/main/java/pl/mczyzewski/recipeapp/reposetories/CategoryRepository.java
+++ src/main/java/pl/mczyzewski/recipeapp/reposetories/CategoryRepository.java
@@ -3,5 +3,11 @@ package pl.mczyzewski.recipeapp.reposetories;
 import org.springframework.data.repository.CrudRepository;
 import pl.mczyzewski.recipeapp.domain.Category;
 
+import java.util.Optional;
+
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+    Optional<Category> findByDescription (String description);
+
 }
