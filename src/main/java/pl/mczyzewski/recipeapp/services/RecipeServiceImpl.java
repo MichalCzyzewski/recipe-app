6854f5d77e0f862ab1d20483d4b18cab29e06ba4
Michal Czyzewski <czy.mich@gmail.com>
2019-10-29 23:26:33 +0100

    add @Data (lombok) annotation

--- src/main/java/pl/mczyzewski/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/pl/mczyzewski/recipeapp/services/RecipeServiceImpl.java
@@ -1,11 +1,13 @@
 package pl.mczyzewski.recipeapp.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.mczyzewski.recipeapp.domain.Recipe;
 import pl.mczyzewski.recipeapp.reposetories.RecipeRepository;
 
 import java.util.HashSet;
 import java.util.Set;
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipe() {
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
