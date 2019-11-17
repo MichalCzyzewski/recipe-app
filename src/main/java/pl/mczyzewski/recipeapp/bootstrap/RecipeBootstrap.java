3c84f7667d84f42c9fc60d6153fca9beba295bbb
Michal Czyzewski <czy.mich@gmail.com>
2019-11-18 00:21:15 +0100

    add full description of View page

--- src/main/java/pl/mczyzewski/recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/pl/mczyzewski/recipeapp/bootstrap/RecipeBootstrap.java
@@ -140,6 +140,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.addIngredient(new Ingredient("Cilantro", new BigDecimal(2), tableSpoonUom));
         guacRecipe.addIngredient(new Ingredient("freshly grated black pepper", new BigDecimal(2), dashUom));
         guacRecipe.addIngredient(new Ingredient("ripe tomato, seeds and pulp removed, chopped", new BigDecimal(".5"), eachUom));
+        guacRecipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simple Recipes");
 
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
